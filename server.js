b1862966665aed1d1914f9730262bea8f1e2a955
daroush <dpeace8@gmail.com>
2018-09-24 18:03:30 -0400

    I connect the frontend to the backend.

--- server.js
+++ server.js
@@ -3,5 +3,11 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.listen(3000, function(){
-  console.log('The server is officially running.');
+  console.log('The server is officially running.');//print to the console
 })
+//app.get('/', (req, res) => res.send('We are the champions'))//print to the website
+
+app.get('/', (req, res) => res.sendfile(__dirname+'/client/index.html'));//connects the backend towards the front end. make sure to plus the folder than contain the index.html
+
+//sets view engine
+app.use(express.static(__dirname+'/client'));
